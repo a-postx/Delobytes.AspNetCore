051c7d09cff0eb2b5f3cfc7e860319ad6388e70f
Алексей Якубин <2412719@mail.ru>
2019-11-09 10:04:49 +0300

    Добавлены новые методы, в.1.1

---
+++ src/Filters/NoLowercaseQueryStringAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace Delobytes.AspNetCore.Filters
+{
+    /// <summary>
+    /// Ensures that an HTTP request URL can contain query string parameters with both upper-case and lower-case
+    /// characters.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
+    public class NoLowercaseQueryStringAttribute : Attribute, IFilterMetadata
+    {
+    }
+}
